c3e649b54407c42139b53cc4f08eb281207be88d
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-10-11 19:11:07 -0300

    requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const url = (iten) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${iten}`)
+.then((Response) => Response.json());
+
+const fetchProducts = async (paran) => {
+  try {
+  const url2 = await url(paran);
+  // const { results } = url2;
+  return url2;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 if (typeof module !== 'undefined') {
